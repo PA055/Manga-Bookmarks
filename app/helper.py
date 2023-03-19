42fcb220b66c09fedd3dda13be40b6a35831ac43
PA055 <praful.adiga@gmail.com>
2023-03-19 12:29:59 -0400

    crawl finalized

--- app/helper.py
+++ app/helper.py
@@ -16,10 +16,10 @@ async def crawlManga4Life(bookmark):
     r = session.get(bookmark.link)
     xml = r.html.find('link[title="RSS Feed"]', first=True).attrs['href']
     xmlr = session.get(xml)
-    chapters = [i.split('\n') for i in xmlr.html.find("item")]
-    idx = chapters[0].lower().find('chapter ')
-    olderChapters = [c for c in chapters if float(c[0][idx+8:]) > bookmark.chapter]
-    return {'id': bookmark.id, 'link': min(olderChapters, key=lambda a: float(a[0][idx+8:]), default=bookmark.link)[1], 'chapter': clean_float(max(olderChapters, key=lambda a: float(a[0][idx+8:]), default=bookmark.chapter)[0][8:]), 'num_chapters': len(olderChapters)}
+    chapters = [i.text.split('\n') for i in xmlr.html.find("item")]
+    idx = chapters[0][0].lower().find('chapter ')
+    olderChapters = [c for c in chapters if float(c[0][idx+8:]) >= bookmark.chapter]
+    return {'id': bookmark.id, 'link': min(olderChapters, key=lambda a: float(a[0][idx+8:]))[1], 'chapter': clean_float(max(chapters, key=lambda a: float(a[0][idx+8:]))[0][idx+8:]), 'num_chapters': len(olderChapters)-1}
 
 
 def crawl_sites(bookmarks):
@@ -27,9 +27,19 @@ def crawl_sites(bookmarks):
         for v in list:
             if v['id'] == id:
                 return v
-    results = asession.run(*[lambda: crawlNitroScans(b) for b in bookmarks if 'https://nitroscans.com' in b.link])
+    results = asession.run(*[lambda: crawlNitroScans(b) for b in bookmarks if 'https://nitroscans.com' in b.link.lower()])
     nitroscans = {i.id: find(i.id, results) for i in bookmarks}
-    manga4life = {i.id: {}}
-    return {i.id: {'link': i.link, 'chapter': 0, 'num chpaters': 0} for i in bookmarks}
+    results = [crawlManga4Life(bookmark) for bookmark in bookmarks if 'https://manga4life.com' in bookmark.link.lower()]
+    manga4life = {i.id: find(i.id, results) for i in bookmarks}
+    all = {}
+    for b in bookmarks:
+        if b.id in nitroscans.keys():
+            all[b.id] = nitroscans[b.id]
+        elif b.id in manga4life.keys():
+            all[b.id] = manga4life[b.id]
+        else:
+            all[b.id] = {'link': i.link, 'chapter': 0, 'num chpaters': 0}
+
+    return all
 
 
