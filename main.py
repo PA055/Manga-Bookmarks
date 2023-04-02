f7d700bb6d81741f5e23f4478499f924e78bb49e
PA055 <praful.adiga@gmail.com>
2023-04-01 21:40:07 -0400

    asyncize

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 from requests_html import AsyncHTMLSession
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import time
-
+from arsenic import get_session, services, browsers
 app = FastAPI()
 
 
@@ -11,10 +10,8 @@ def clean_float(f):
     return int(f) if float(f) == int(f) else float(f)
 
 
-@app.get("/manga")
-async def manga():
+async def manga(bookmark):
     session = AsyncHTMLSession()
-    bookmark = {'id': 2, 'link': 'https://manga4life.com/manga/A-Rank-Party-wo-Ridatsu-Shita-Ore-wa', 'chapter': 43}
     r = await session.get(bookmark['link'])
     xml = r.html.find('link[title="RSS Feed"]', first=True).attrs['href']
     xmlr = await session.get(xml)
@@ -25,19 +22,39 @@ async def manga():
         return {'id': bookmark['id'], 'link': bookmark['link'], 'chapter': bookmark['chapter'], 'num_chapters': 0}
     return {'id': bookmark['id'], 'link': min(olderChapters, key=lambda a: float(a[0][idx+8:]))[1], 'chapter': clean_float(max(chapters, key=lambda a: float(a[0][idx+8:]))[0][idx+8:]), 'num_chapters': len(olderChapters)}
 
+@app.get('/mangas')
+async def mangas():
+    bookmarks = [
+        {'id': 10, 'link': 'https://manga4life.com/manga/A-Rank-Party-wo-Ridatsu-Shita-Ore-wa', 'chapter': 43},
+        {'id': 11, 'link': 'https://manga4life.com/manga/Maou-sama-No-Machizukuri', 'chapter': 19},
+        {'id': 12, 'link': '', 'chapter': 0},
+        {'id': 13, 'link': '', 'chapter': 0},
+        {'id': 14, 'link': '', 'chapter': 0},
+        {'id': 15, 'link': '', 'chapter': 0},
+        {'id': 16, 'link': '', 'chapter': 0},
+        {'id': 17, 'link': '', 'chapter': 0},
+        {'id': 18, 'link': '', 'chapter': 0},
+        {'id': 19, 'link': '', 'chapter': 0},
+    ]
 
-@app.get("/nitro")
-def nitro():
-    bookmark = {'id': 1, 'link': 'https://www.nitroscans.com/series/i-stole-the-number-one-rankers-soul/', 'chapter': 14}
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(options=options)
-    driver.get(bookmark['link'])
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    chapters = [a for a in soup.findAll('a') if 'chapter ' in a.text.lower()]
+    results = []
+    for bookmark in bookmarks:
+        results.append(await manga(bookmark))
+
+    return results
+
+
+async def nitro(bookmark):
+    service = services.Chromedriver(binary='./chromedriver')
+    browser = browsers.Chrome()
+    browser.capabilities = {
+        "goog:chromeOptions": {"args": ["--headless", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage"]}
+    }
+    async with get_session(service, browser) as session:
+        await session.get(bookmark['link'])
+        chapters = await session.get_elements("ul li.wp-manga-chapter a")
+        # soup = BeautifulSoup(driver.page_source, 'html.parser')
+    # chapters = [a for a in soup.findAll('a') if 'chapter ' in a.text.lower()]
     olderChapters = [a for a in chapters if float(a.text.strip()[8:]) > bookmark['chapter']]
     if len(olderChapters) == 0:
         return {'id': bookmark['id'], 'link': bookmark['link'], 'chapter': bookmark['chapter'], 'num_chapters': 0}
@@ -47,5 +64,27 @@ def nitro():
             'chapter': clean_float(max(chapters, key=lambda a: float(a.text.strip()[8:])).text.strip()[8:]), 
             'num_chapters': len(olderChapters)
         }
+
+
+@app.get("/nitros")
+async def nitros():
+    bookmarks = [
+        {'id': 1, 'link': 'https://www.nitroscans.com/series/i-stole-the-number-one-rankers-soul/', 'chapter': 14},
+        {'id': 2, 'link': 'https://nitroscans.com/series/demonic-emperor/', 'chapter': 350},
+        {'id': 3, 'link': 'https://nitroscans.com/series/the-return-of-the-prodigious-swordmaster/', 'chapter': 5},
+        {'id': 4, 'link': 'https://nitroscans.com/series/the-golden-lion-king/', 'chapter': 0},
+        {'id': 5, 'link': 'https://nitroscans.com/series/gangho-apocalypse/', 'chapter': 5},
+        {'id': 6, 'link': 'https://nitroscans.com/series/the-great-demon-king/', 'chapter': 2},
+        {'id': 7, 'link': 'https://nitroscans.com/series/martial-peak/', 'chapter': 3000},
+        {'id': 8, 'link': 'https://nitroscans.com/series/top-tier-providence-secretly-cultivate-for-a-thousand-years/', 'chapter': 90},
+        {'id': 9, 'link': 'https://nitroscans.com/series/earthlings-are-insane/', 'chapter': 121},
+    ]
+    results = []
+    for bookmark in bookmarks:
+        result = await nitro(bookmark)
+        results.append(result)
+
+    return results
+    
 
 # https://medium.com/analytics-vidhya/asynchronous-web-scraping-101-fetching-multiple-urls-using-arsenic-ec2c2404ecb4
--- test.py
+++ test.py
@@ -1,22 +1,84 @@
+
+from requests_html import AsyncHTMLSession
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import time
+import asyncio, logging, structlog
+from arsenic import get_session, services, browsers
+from colorama import Fore
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
+def set_arsenic_log_level(level = logging.ERROR):
+    logger = logging.getLogger('arsenic')
 
-driver = webdriver.Chrome(options=options)
-driver.get("https://nitroscans.com/series/i-stole-the-number-one-rankers-soul")
+    def logger_factory():
+        return logger
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-chapters = [a for a in soup.findAll('a') if 'chapter ' in a.text.lower()]
+    structlog.configure(logger_factory=logger_factory)
+    logger.setLevel(level)
 
-while len(chapters) <= 0:
-    time.sleep(1)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    chapters = [a for a in soup.findAll('a') if 'chapter ' in a.text.lower()]
+async def nitro(bookmark):
+    service = services.Chromedriver(binary='./chromedriver')
+    browser = browsers.Chrome()
+    browser.capabilities = {
+        "goog:chromeOptions": {"args": ["--headless", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage"]}
+    }
+    async with get_session(service, browser) as session:
+        await session.get(bookmark['link'])
+        await session.wait_for_element(20, 'ul li.wp-manga-chapter a')
+        chapters = await session.get_elements("ul li.wp-manga-chapter a")
 
-print(chapters)
+        link = bookmark['link']
+        latest_chapter = bookmark['chapter']
+        num_chapters = 0
+
+        for chapter in chapters:
+            chapter_text = await chapter.get_text()
+            try:
+                chapter_num = float(chapter_text.strip()[8:])
+            except ValueError:
+                chapter_link = await chapter.get_attribute('href')
+                chapter_num = float(chapter_link.split('/chapter-')[-1][:-1].replace('-', '.'))
+
+            if chapter_num > bookmark['chapter']:
+                if num_chapters == 0:
+                    link = await chapter.get_attribute('href')
+                num_chapters += 1
+            else:
+                continue
+
+            if chapter_num > latest_chapter:
+                latest_chapter = chapter_num
+
+
+        return {
+                'id': bookmark['id'], 
+                'link': link, 
+                'chapter': latest_chapter,
+                'num_chapters': num_chapters, 
+            }
+
+
+async def run():
+    bookmarks = [
+        {'id': 1, 'link': 'https://www.nitroscans.com/series/i-stole-the-number-one-rankers-soul/', 'chapter': 14},
+        {'id': 2, 'link': 'https://nitroscans.com/series/demonic-emperor/', 'chapter': 350},
+        {'id': 3, 'link': 'https://nitroscans.com/series/the-return-of-the-prodigious-swordmaster/', 'chapter': 5},
+        {'id': 4, 'link': 'https://nitroscans.com/series/the-golden-lion-king/', 'chapter': 0},
+        {'id': 5, 'link': 'https://nitroscans.com/series/gangho-apocalypse/', 'chapter': 5},
+        {'id': 6, 'link': 'https://nitroscans.com/series/the-great-demon-king/', 'chapter': 2},
+        # {'id': 7, 'link': 'https://nitroscans.com/series/martial-peak/', 'chapter': 3000},
+        {'id': 8, 'link': 'https://nitroscans.com/series/top-tier-providence-secretly-cultivate-for-a-thousand-years/', 'chapter': 90},
+        {'id': 9, 'link': 'https://nitroscans.com/series/earthlings-are-insane/', 'chapter': 121},
+    ]
+    results = []
+    for bookmark in bookmarks:
+        results.append(asyncio.create_task(nitro(bookmark)))
+
+    list_of_results = await asyncio.gather(*results)
+    return list(results)
+
+if __name__ == '__main__':
+    set_arsenic_log_level()
+    df = asyncio.run(run())
+    for result in df:
+        print(result)
+
