b8879d56102f9fbada813e5aeeac403c4b355795
PA055 <praful.adiga@gmail.com>
2023-04-03 09:20:38 -0400

    integrated them together

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from app import api_app
 from app import app, db
 from app.models import *
 from app.helper import *
+import uvicorn
+
+if __name__ == '__main__':
+    uvicorn.run(api_app, port=8000)
--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,29 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+from app.api_db import Base, engine, SessionLocal
+
+
+Base.metadata.create_all(bind=engine)
+
+api_app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+from app import api_routes, api_models
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
+
+api_app.mount("/", WSGIMiddleware(app))
